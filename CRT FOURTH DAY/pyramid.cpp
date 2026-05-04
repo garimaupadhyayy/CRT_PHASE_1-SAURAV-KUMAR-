8e3c2dcf670b14141284b1394cfeb800066fee4a
garimaupadhyayy <garima.upadhyay716@gmail.com>
2026-05-05 00:21:55 +0530

    Normal commit

---
+++ CRT FOURTH DAY/pyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+  int r;
+  cin>>r;
+  for(int i = 1; i<=r; i++)
+  {
+    for(int j = 1; j<=r-i; j++)
+    {
+      cout<< " ";
+    }
+      for(int k = 1; k<=i; k++)
+      {
+        cout<<k<<" ";
+      }
+      for(int g = i-1; g>=1; g--)
+      {
+        cout<< g<<" ";
+          }
+     cout<<endl;
+        }
+  return 0;
+}
