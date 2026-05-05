0172ed4f6653ddbee1e4e78497deef801ed60e6b
garimaupadhyayy <garima.upadhyay716@gmail.com>
2026-05-05 19:03:41 +0530

    Normal commit

---
+++ CRT FIFTH DAY/area of rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+double areaofrectangle(double a, double b)
+{
+  return a*b;
+}
+
+int main()
+{
+  double result = areaofrectangle(3.8575,7.8738);
+  cout<<result;
+  return 0;
+}
